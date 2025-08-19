442d18d58b2407ccc59d224edbe0084872ab7ca3
anurag-kkumar <anukumar95084@gmail.com>
2025-08-19 11:08:45 +0530

    "[Anurag].add q16"

---
+++ CoreJava/Java_OOP/Main2.java
@@ -0,0 +1,16 @@
+package CoreJava.Java_OOP;
+
+import com.sun.tools.javac.Main;
+
+public class Main2 {
+    // if local variable not  initialized then it print defult val
+
+    int nums;
+
+
+    public static void main(String[] args) {
+        Main2 main=new Main2();
+        System.out.println(main.nums);
+    }
+}
+
