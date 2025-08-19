b13d698ddb40041af164b977eccba163c13f9b5c
anurag-kkumar <anukumar95084@gmail.com>
2025-08-19 10:48:24 +0530

    "[Anurag].add q12"

---
+++ CoreJava/Java_OOP/User2.java
@@ -0,0 +1,24 @@
+package CoreJava.Java_OOP;
+
+public class User2 {
+    int localvar;
+    final int fixe=0;
+    User2(int instanceVar,int fixe){
+        this.localvar=instanceVar;
+        fixe.this=fixe;
+        //error : final valur cannot change
+//        java: cannot find symbol
+//        symbol:   class fixe
+//  location: class CoreJava.Java_OOP.User2
+    }
+    void display()
+    {
+        System.out.println(fixe);
+        System.out.println(localvar);
+    }
+    public static void main(String[] args) {
+        User2 dmo=new User2(1,22);
+
+        dmo.display();
+    }
+}
