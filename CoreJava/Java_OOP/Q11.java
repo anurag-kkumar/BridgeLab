118294b0754d62f03c810841a47fae553950af92
anurag-kkumar <anukumar95084@gmail.com>
2025-08-19 10:40:33 +0530

    "[Anurag].add q11"

---
+++ CoreJava/Java_OOP/Q11.java
@@ -0,0 +1,11 @@
+package CoreJava.Java_OOP;
+
+public class Q11 {
+    static int defaultval;
+
+    public static void main(String[] args) {
+        System.out.println(defaultval);
+        defaultval=20;
+        System.out.println(defaultval);
+    }
+}
