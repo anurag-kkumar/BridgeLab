96ae8051097509c51c7fb67dfd6dd8805a03406e
anurag-kkumar <anukumar95084@gmail.com>
2025-08-19 10:41:11 +0530

    "[Anurag].add q9"

--- CoreJava/Java_OOP/Account.java
+++ CoreJava/Java_OOP/Account.java
@@ -1,5 +1,5 @@
 package CoreJava.Java_OOP;
-
+// 9. Create a class Account with instance variables: accountHolder, balance. Use default values. Print them without setting any values.
 public class Account {
     String accountHolder;
     int balance;
