ac041b7d010e1e719486215a4d8bcee7b7857b5b
anurag-kkumar <anukumar95084@gmail.com>
2025-08-19 10:42:37 +0530

    "[Anurag].add use final keyword"

--- CoreJava/Java_OOP/Demo1.java
+++ CoreJava/Java_OOP/Demo1.java
@@ -3,18 +3,15 @@ package CoreJava.Java_OOP;
 public class Demo1 {
     int localvar;
     final int fixed=0;
-    Demo1(int instanceVar,int fixed){
+    Demo1(int instanceVar){
         this.localvar=instanceVar;
-        this.fixed=fixed;
-        // complie error :cannot assign a value to final variable fixed
     }
     void display()
     {
-        System.out.println(fixed);
         System.out.println(localvar);
     }
     public static void main(String[] args) {
-        Demo1 dmo=new Demo1(1,30);
+        Demo1 dmo=new Demo1(1);
 
         dmo.display();
     }
