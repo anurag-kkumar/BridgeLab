e145c8d568671cecf1358e61540e9808cc400ff7
anurag-kkumar <anukumar95084@gmail.com>
2025-08-19 10:29:26 +0530

    "[Anurag].add static block"

---
+++ CoreJava/Java_OOP/Qutiion8.java
@@ -0,0 +1,14 @@
+package CoreJava.Java_OOP;
+
+public class Qutiion8 {
+    static  int roll;
+    static String name;
+   static  {
+        roll=23;
+        name="anurag";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(roll+" "+name);
+    }
+}
