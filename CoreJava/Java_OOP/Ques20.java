b6618fdcff073048aac8afb720e9f35cb3678d1a
anurag-kkumar <anukumar95084@gmail.com>
2025-08-19 11:20:13 +0530

    "[Anurag].add q20"

---
+++ CoreJava/Java_OOP/Ques20.java
@@ -0,0 +1,10 @@
+package CoreJava.Java_OOP;
+//20. Create a program where you access a static variable without creating an object of the class.
+public class Ques20 {
+    static int val;
+
+    public static void main(String[] args) {
+        System.out.println(val=20);
+    }
+//    static var not need to creat their obj
+}
