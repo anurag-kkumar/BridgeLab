89342434da859cbc26c4814953f65e42883d682f
anurag-kkumar <anukumar95084@gmail.com>
2025-08-25 11:46:35 +0530

    "[Anurag].add q29"

---
+++ CoreJava/Java_String/duplicate.java
@@ -0,0 +1,35 @@
+package CoreJava.Java_String;
+
+import java.util.Scanner;
+
+public class duplicate {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String s=in.next();
+        String output = duplicates(s);
+        System.out.println( output);
+    }
+        public static String duplicates(String input) {
+            if (input == null || input.length() == 0) {
+                return input;
+            }
+
+            StringBuilder ans = new StringBuilder();
+            char pChar = input.charAt(0);
+            ans.append(pChar);
+
+            for (int i = 1; i < input.length(); i++) {
+                char cchar = input.charAt(i);
+                if (cchar != pChar) {
+                    ans.append(cchar);
+                    pChar = cchar;
+                }
+            }
+
+            return ans.toString();
+        }
+
+
+    }
+
+
