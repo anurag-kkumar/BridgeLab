c3ae032b877ca1c97f824d490ef66784afd96d9c
anurag-kkumar <135259732+anurag-kkumar@users.noreply.github.com>
2025-08-27 11:39:09 +0530

    Update DateArithmetic.java

--- CoreJava/Extras/DateArithmetic.java
+++ CoreJava/Extras/DateArithmetic.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 
 public class DateArithmetic {
     public static void main(String[] args) {
-        performDateArithmetic();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("date: YYYY-MM-DD  ");
+        String str = scanner.nextLine();
+        datechanges(str);
     }
 
-    static void performDateArithmetic() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a date (YYYY-MM-DD): ");
-
-        String str = scanner.nextLine();
+    static void datechanges(String str) {
+        
         LocalDate    date = LocalDate.parse(str);
-        LocalDate modifiedDate = date.plusDays(7).plusMonths(1).plusYears(2);
-        LocalDate finalDate = modifiedDate.minusWeeks(3);
+        LocalDate newdate = date.plusDays(7).plusMonths(1).plusYears(2);
+        LocalDate finalDate = newdate.minusWeeks(3);
         System.out.println(finalDate);
     }
 }
