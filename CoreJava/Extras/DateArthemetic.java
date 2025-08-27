c67a59c7cc05391c3968b70a67f2121304a401d4
anurag-kkumar <anukumar95084@gmail.com>
2025-08-27 10:28:35 +0530

    "[Anurag].add q2 "

---
+++ CoreJava/Extras/DateArthemetic.java
@@ -0,0 +1,23 @@
+package CoreJava.Extras;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class DateArthemetic {
+    public static void main(String[] args) {
+        dateArithmetic();
+    }
+    static void dateArithmetic() {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter YYYY-MM-DD  ");
+        String input = scanner.nextLine();
+          LocalDate date = LocalDate.parse(input);
+        LocalDate newdate = date.plusDays(7).plusMonths(1).plusYears(2);
+        LocalDate finaldate = newdate.minusWeeks(3);
+
+        System.out.println(finaldate);
+
+    }
+}
