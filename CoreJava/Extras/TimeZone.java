de130f630cc632d122a67c77f6e829eca9981127
anurag-kkumar <135259732+anurag-kkumar@users.noreply.github.com>
2025-08-27 11:45:32 +0530

    Update TimeZone.java

--- CoreJava/Extras/TimeZone.java
+++ CoreJava/Extras/TimeZone.java
@@ -18,30 +18,10 @@ public class TimeZone {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
 
-        // Time zones to display
-
-
         ZoneId zoneId = ZoneId.of(zones);
         ZonedDateTime zonedDateTime = ZonedDateTime.now(zoneId);
         System.out.println(zones + ": " + zonedDateTime.format(formatter));
     }
 
-    public static class DateArthemetic {
-        public static void main(String[] args) {
-            dateArithmetic();
-        }
-        static void dateArithmetic() {
-            Scanner scanner = new Scanner(System.in);
 
-
-            System.out.print("Enter YYYY-MM-DD  ");
-            String input = scanner.nextLine();
-              LocalDate date = LocalDate.parse(input);
-            LocalDate newdate = date.plusDays(7).plusMonths(1).plusYears(2);
-            LocalDate finaldate = newdate.minusWeeks(3);
-
-            System.out.println(finaldate);
-
-        }
-    }
 }
