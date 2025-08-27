b3cd07c21a6dfc7abbf524f77c42040eeb00106d
anurag-kkumar <anukumar95084@gmail.com>
2025-08-27 10:29:19 +0530

    "[Anurag].add q1 "

---
+++ CoreJava/Extras/TimeZone.java
@@ -0,0 +1,27 @@
+package CoreJava.Extras;
+
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class TimeZone {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("GMT Asia/Kolkata America/Los_Angeles" );
+        String strc=in.next();
+        displayDifferentTimezones(strc);
+    }
+
+    static void displayDifferentTimezones(String zones) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+
+        // Time zones to display
+
+
+        ZoneId zoneId = ZoneId.of(zones);
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(zoneId);
+        System.out.println(zones + ": " + zonedDateTime.format(formatter));
+    }
+}
