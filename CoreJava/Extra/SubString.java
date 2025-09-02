0f4d3d22405e3febca8f311d067e52aacfd694a6
anurag-kkumar <135259732+anurag-kkumar@users.noreply.github.com>
2025-09-02 09:23:24 +0530

    q6

---
+++ CoreJava/Extra/SubString.java
@@ -0,0 +1,20 @@
+package CoreJava.Java_String;
+
+import java.util.Arrays;
+
+public class SubString {
+    class abc{
+        int a;
+        int b;
+        abc(){
+            this.a=10;
+            this.b=20;
+        }
+
+
+    }
+    public static void main(String[] args) {
+        abc[]abc=new abc[2];
+        System.out.println(Arrays.toString(abc));
+    }
+}
